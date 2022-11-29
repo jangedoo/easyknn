5046b08735bc7dbdbcc359e87bf6ad75a89a6da0
Sanjaya Subedi <sanjaya.subedi@deliveryhero.com>
2022-11-29 21:48:45 +0100

    patch release

--- easyknn/__init__.py
+++ easyknn/__init__.py
@@ -1,6 +1,6 @@
 from easyknn.knn import AnnoyBackend, EasyKNN, EmbeddingsIndexBuilder, SKLearnKNNBackend
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "EmbeddingsIndexBuilder",
