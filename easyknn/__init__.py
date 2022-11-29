73fc372798e43a60db2e0049c449c408890b62c1
Sanjaya Subedi <sanjaya.subedi@deliveryhero.com>
2022-11-29 21:21:33 +0100

    bump version to 0.3.0

--- easyknn/__init__.py
+++ easyknn/__init__.py
@@ -1,6 +1,6 @@
 from easyknn.knn import AnnoyBackend, EasyKNN, EmbeddingsIndexBuilder, SKLearnKNNBackend
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "EmbeddingsIndexBuilder",
