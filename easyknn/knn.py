6cb49f9bfa380bf1acbb5a90cb7d323f8b265dee
Sanjaya Subedi <jangedoo@gmail.com>
2024-05-02 15:55:52 +0200

    add helper functions specific to backend to build index

--- easyknn/knn.py
+++ easyknn/knn.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pickle
 from pathlib import Path
-from typing import Type
+from typing import Literal, Type
 
 
 class KNNBackend:
@@ -255,3 +255,46 @@ class EasyKNN:
             init_kwargs=init_kwargs, fit_kwargs=fit_kwargs, backend_cls=backend_cls
         )
         return cls(index=index, item2idx=builder.item2idx)
+
+    @classmethod
+    def from_builder_with_sklearn(
+        cls,
+        builder: EmbeddingsIndexBuilder,
+        n_neighbors: int = 5,
+        metric: str = "cosine",
+        radius: float = 1.0,
+        algorithm: Literal["auto", "ball_tree", "kd_tree", "brute"] = "auto",
+        init_kwargs: dict | None = None,
+    ):
+        init_kwargs = (init_kwargs or {}).update(
+            {
+                "n_neighbors": n_neighbors,
+                "metric": metric,
+                "radius": radius,
+                "algorithm": algorithm,
+            }
+        )
+        return cls.from_builder(
+            builder=builder, init_kwargs=init_kwargs, fit_kwargs=None, backend="sklearn"
+        )
+
+    @classmethod
+    def from_builder_with_annoy(
+        cls,
+        builder: EmbeddingsIndexBuilder,
+        metric: Literal[
+            "angular", "euclidean", "manhattan", "hamming", "dot"
+        ] = "euclidean",
+        n_trees: int = 10,
+        n_jobs: int = -1,
+    ):
+
+        init_kwargs = {"metric": metric}
+        fit_kwargs = {"n_trees": n_trees, "n_jobs": n_jobs}
+
+        return cls.from_builder(
+            builder=builder,
+            init_kwargs=init_kwargs,
+            fit_kwargs=fit_kwargs,
+            backend="annoy",
+        )
--- tests/test_easyknn.py
+++ tests/test_easyknn.py
@@ -5,12 +5,16 @@ import pytest
 import easyknn
 
 
-@pytest.mark.parametrize("backend", ["annoy", "sklearn"])
-def test_neighbors(backend: str):
+@pytest.fixture
+def index_builder() -> easyknn.EmbeddingsIndexBuilder:
     builder = easyknn.EmbeddingsIndexBuilder()
     builder.add([[1.0, 2.0], [5.0, 1.0], [1, 2.5]], items=["a", "b", "c"])
+    return builder
 
-    knn = easyknn.EasyKNN.from_builder(builder=builder, backend=backend)
+
+@pytest.mark.parametrize("backend", ["annoy", "sklearn"])
+def test_neighbors(backend: str, index_builder: easyknn.EmbeddingsIndexBuilder):
+    knn = easyknn.EasyKNN.from_builder(builder=index_builder, backend=backend)
     items, distances = knn.neighbors(vector=[1.0, 2.0], k=2)
     assert items == ["a", "c"]
     assert len(distances) == 2
@@ -25,11 +29,8 @@ def test_neighbors(backend: str):
 
 
 @pytest.mark.parametrize("backend", ["annoy", "sklearn"])
-def test_save_an_load(backend: str):
-    builder = easyknn.EmbeddingsIndexBuilder()
-    builder.add([[1.0, 2.0], [5.0, 1.0], [1, 2.5]], items=["a", "b", "c"])
-
-    knn = easyknn.EasyKNN.from_builder(builder=builder, backend=backend)
+def test_save_an_load(backend: str, index_builder: easyknn.EmbeddingsIndexBuilder):
+    knn = easyknn.EasyKNN.from_builder(builder=index_builder, backend=backend)
 
     with tempfile.TemporaryDirectory() as tempdir:
         knn.save(path=tempdir)
@@ -40,3 +41,21 @@ def test_save_an_load(backend: str):
 
     assert nbors == nbors2
     assert len(dists) == len(dists2)
+
+
+def test_from_builder_with_sklearn(index_builder: easyknn.EmbeddingsIndexBuilder):
+    knn = easyknn.EasyKNN.from_builder_with_sklearn(
+        builder=index_builder, n_neighbors=2
+    )
+    assert isinstance(knn.index, easyknn.SKLearnKNNBackend)
+    items, distances = knn.neighbors(vector=[1.0, 2.0], k=2)
+    assert items == ["a", "c"]
+    assert len(distances) == 2
+
+
+def test_from_builder_with_annoy(index_builder: easyknn.EmbeddingsIndexBuilder):
+    knn = easyknn.EasyKNN.from_builder_with_annoy(builder=index_builder)
+    assert isinstance(knn.index, easyknn.AnnoyBackend)
+    items, distances = knn.neighbors(vector=[1.0, 2.0], k=2)
+    assert items == ["a", "c"]
+    assert len(distances) == 2
